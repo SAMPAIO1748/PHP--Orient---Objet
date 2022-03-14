15b8397fe24fb3f093d181e9905e02f9b2829d9a
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-14 13:56:44 +0100

    exo03

---
+++ 03-manipulation-type-argument/exercice03.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Schéma des classes (UML) :
+ * 
+ * -------------------------
+ * Vehicule
+ * -------------------------
+ * setlitresEssence()
+ * getlitresEssence()
+ * -------------------------
+ * 
+ * 
+ * 
+ * 
+ * -------------------------
+ * Pompe
+ * -------------------------
+ * setlitresEssence()
+ * getlitresEssence()
+ * donnerEssence()
+ * 
+ * 
+ * Consigne :
+ * 1. Création d'un véhicule 1
+ * 2. Attribuer un nombre de litre d'essence au véhicule 1 : 5
+ * 3. Afficher le nombre de litres d'essence du véhicule 1
+ * 4. Création d'une pompe
+ * 5. Attribuer un nombre de litres d'essence à la pompe : 800
+ * 6. Afficher le nombre de litres d'essence de la pompe.
+ * 7. La pompe donne de l'essence en faisant le plein (50L) au véhicule 1
+ * 8. Afficher le nombre de litres restant d'essence de la pompe
+ * 9. Affcher le nombre de litres d'essence du véhicule 1
+ * 10. Faire en sorte que la véhicule ne puisse pas contenir plus de 50L d'essence (limite du réservoir)
+ * 
+ */
