db7f833e32b887c189b7fc28a72d95b3cba483d1
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-14 11:47:49 +0100

    exo get set

---
+++ 02-getter-setter-constructeur-this/Membre.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/*******
+ * class Membre
+ * {
+ * 
+ *  private $pseudo;
+ *  private $mdp;
+ *  private $role;
+ * 
+ * }
+ * 
+ * Créer les getters et les setters qui vont bien avec cette classe
+ * 
+ * Instancier deux membres différents et donner des valeurs à leurs propriétés et les afficher
+ * 
+ */
