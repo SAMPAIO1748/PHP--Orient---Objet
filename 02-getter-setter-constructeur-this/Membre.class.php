f50c6e33f0de497f9dd6d129e3d40dc5fe64abf3
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-14 12:23:11 +0100

    correction exo

--- 02-getter-setter-constructeur-this/Membre.class.php
+++ 02-getter-setter-constructeur-this/Membre.class.php
@@ -15,3 +15,60 @@
  * Instancier deux membres différents et donner des valeurs à leurs propriétés et les afficher
  * 
  */
+
+class Membre
+{
+
+    private $pseudo;
+    private $mdp;
+    private $role;
+
+    public function setPseudo($arg)
+    {
+        if (is_string($arg)) {
+            $this->pseudo = $arg;
+        }
+    }
+
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    public function setMdp($arg)
+    {
+        $this->mdp = $arg;
+    }
+
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    public function setRole($arg)
+    {
+        $this->role = $arg;
+    }
+
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
+
+$membre1 = new Membre;
+$membre1->setPseudo("Patrick");
+$membre1->setMdp("toto");
+$membre1->setRole("admin");
+echo "Pseudo : " . $membre1->getPseudo() . '<br>';
+echo "Mdp : " . $membre1->getMdp() . '<br>';
+echo "Role : " . $membre1->getRole() . '<br>';
+
+
+$membre2 = new Membre;
+$membre2->setPseudo("Stéphane");
+$membre2->setMdp("tata");
+$membre2->setRole("user");
+echo "Pseudo : " . $membre2->getPseudo() . '<br>';
+echo "Mdp : " . $membre2->getMdp() . '<br>';
+echo "Role : " . $membre2->getRole() . '<br>';
