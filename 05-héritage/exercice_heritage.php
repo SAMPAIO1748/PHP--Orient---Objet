4ebcb12018f597255246526efb402143d075097f
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-15 12:08:53 +0100

    exercice héritage

---
+++ 05-héritage/exercice_heritage.php
@@ -0,0 +1,61 @@
+<?php
+
+// Créer les classes en fonction des éléments suivants :
+
+/*********************
+ * 
+ * 
+ * -------------------
+ * Voiture
+ * -------------------
+ * $moteur
+ * $portes
+ * -------------------
+ * jeRoule()
+ * -------------------
+ * 
+ * 
+ * -------------------
+ * Audi
+ * -------------------
+ * $couleur
+ * $annee
+ * $modele
+ * 
+ * __construct($moteur, $portes, $couleur, $annee, $modele)
+ * getter et setter des propriétés
+ * 
+ * -------------------
+ * BMW
+ * -------------------
+ * $couleur
+ * $annee
+ * $modele
+ * 
+ * __construct($moteur, $portes, $couleur, $annee, $modele)
+ * getter et setter des propriétés
+ * 
+ * -------------------
+ * Mercedes-Benz
+ * -------------------
+ * $couleur
+ * $annee
+ * $modele
+ * 
+ * __construct($moteur, $portes, $couleur, $annee, $modele)
+ * getter et setter des propriétés
+ * 
+ */
+
+// Audi, BMW et Mercedez-benz héritent de Voiture
+
+/***
+ * 
+1. Créer 3 audi
+2. Afficher la couleur des 3 audis
+3. Afficher le modèle des 3 audis
+4.  Afficher l'année des 3 audis
+5. Afficher le moteur des 3 audis
+6. Afficher les portes des 3 audis
+7. Faire de même pour 3 bmw et 3 mercedes-benz
+ */
