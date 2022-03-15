f48f4ac167a819cf9d4e261c81c479ff022524ab
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-15 09:55:18 +0100

    heritage animal

--- 05-héritage/Animal.php
+++ 05-héritage/Animal.php
@@ -13,6 +13,6 @@ class Animal
     }
 }
 
-// exercie : créer deux classes : Elephant et Chat
+// exercie : créer deux classes : Elephant et Chat qui héritent de Animal
 // chaque classe aura la méthode quiSuisJe()
 // la classe chat aura la méthode manger (qui retoutrne je mange comme un chat).
