74a4dcacda5f0ff1a34ac722e4a2b42ee0d0e461
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-17 11:37:21 +0100

    appel_namespace

--- 09-namespace/appel_namespace_commerce.php
+++ 09-namespace/appel_namespace_commerce.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once('namespace_commerce.php'); // on appelle le fichier namespace_commerce.php
-use Commerce2, Commerce3; // permet de dire quels namespaces on veut importer.
+use Commerce2, Commerce3, Commerce1; // permet de dire quels namespaces on veut importer.
 
-$produit2 = new Commerce2\Produit;
+$produit2 = new Commerce2\Produit; // Il ne faut pas oublier le namespace et le \ sinon la classe ne sera pas identifiée
 var_dump($produit2);
 
 echo "<hr>";
@@ -14,3 +14,20 @@ var_dump($produit3);
 echo "<hr>";
 
 // Exercice : créer $panier basé sur la classe Panier et $commande basé sur la classe Commande et afficher les objets avec des var_dump()
+
+$panier = new Commerce3\Panier;
+var_dump($panier);
+
+echo "<hr>";
+
+$commande = new Commerce1\Commande;
+var_dump($commande);
+
+/**
+ * 
+ * Les namespaces (ou espaces de nom) permettent de ranger des classes qui pourront avoir des noms identiques
+ * Cela est très pratique lors d'un travail collaboratif pour éviter que des fonctions ou des classes soit identiques.
+ * Cela évite des problèmes dans le code du projet.
+ * 
+ * 
+ */
