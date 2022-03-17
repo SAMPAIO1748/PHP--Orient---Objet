e88b35494b078554ae8342a29e96e9b43c5c3b55
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-17 10:11:29 +0100

    intro_namespace

---
+++ 09-namespace/namespace_intro.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Erreur fatale : on ne peut pas déclarer deux classes ou deux focntion de même nom !!!! INTERDIT, VERBOTEN, FORBIDDEN !!!!!
+ * 
+ * 
+ * class A
+ *{
+ *}
+
+ *class A
+ *{
+ *}
+
+ *function a()
+ *{
+ *};
+ *function a()
+ *{
+ *};
+ * 
+ * 
+ */
